dc095662cfb7abde850a4a29109d857db705c5b9
Mario Valente <mario.valente@ifood.com.br>
2026-02-26 12:25:19 -0300

    feat: add list and get rolebinding

--- ccloud/iam_role_bindings.go
+++ ccloud/iam_role_bindings.go
@@ -21,6 +21,64 @@ type RoleBinding struct {
 	CrnPattern string `json:"crn_pattern"`
 }
 
+type RoleBindingList struct {
+	common.BaseModel
+	Data []RoleBinding `json:"data"`
+}
+
+type ListRoleBindingsQuery struct {
+	common.PaginationOptions
+	Principal  string `url:"principal,omitempty"`
+	RoleName   string `url:"role_name,omitempty"`
+	CrnPattern string `url:"crn_pattern,omitempty"`
+}
+
+func (c *ConfluentClient) ListRoleBindings(query *ListRoleBindingsQuery) (*RoleBindingList, error) {
+	urlPath := "/iam/v2/role-bindings"
+
+	res, err := c.doRequest(urlPath, http.MethodGet, nil, query)
+	if err != nil {
+		return nil, err
+	}
+
+	if http.StatusOK != res.StatusCode {
+		return nil, fmt.Errorf("failed to list role bindings: %s", res.Status)
+	}
+
+	defer res.Body.Close()
+
+	var roleBindingList RoleBindingList
+	err = json.NewDecoder(res.Body).Decode(&roleBindingList)
+	if err != nil {
+		return nil, err
+	}
+
+	return &roleBindingList, nil
+}
+
+func (c *ConfluentClient) GetRoleBinding(roleBindingId string) (*RoleBinding, error) {
+	urlPath := fmt.Sprintf("/iam/v2/role-bindings/%s", roleBindingId)
+
+	res, err := c.doRequest(urlPath, http.MethodGet, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if http.StatusOK != res.StatusCode {
+		return nil, fmt.Errorf("failed to get role binding: %s", res.Status)
+	}
+
+	defer res.Body.Close()
+
+	var roleBinding RoleBinding
+	err = json.NewDecoder(res.Body).Decode(&roleBinding)
+	if err != nil {
+		return nil, err
+	}
+
+	return &roleBinding, nil
+}
+
 func (c *ConfluentClient) CreateRoleBinding(req *RoleBindingCreateReq) (*RoleBinding, error) {
 	urlPath := "/iam/v2/role-bindings"
 
--- ccloud/iam_role_bindings_test.go
+++ ccloud/iam_role_bindings_test.go
@@ -76,3 +76,125 @@ func TestCreateRoleBindingError(t *testing.T) {
 	assert.Nil(t, result)
 	assert.Contains(t, err.Error(), "failed to create role binding")
 }
+
+func TestListRoleBindings(t *testing.T) {
+	expectedList := &ccloud.RoleBindingList{
+		BaseModel: common.BaseModel{
+			ApiVersion: "iam/v2",
+			Kind:       "RoleBindingList",
+		},
+		Data: []ccloud.RoleBinding{
+			{
+				BaseModel: common.BaseModel{
+					ApiVersion: "iam/v2",
+					Kind:       "RoleBinding",
+					Id:         "rb-001",
+				},
+				Principal:  "User:u-111aaa",
+				RoleName:   "CloudClusterAdmin",
+				CrnPattern: "crn://confluent.cloud/organization=1111aaaa-11aa-11aa-11aa-111111aaaaaa/environment=env-aaa1111/cloud-cluster=lkc-1111aaa",
+			},
+			{
+				BaseModel: common.BaseModel{
+					ApiVersion: "iam/v2",
+					Kind:       "RoleBinding",
+					Id:         "rb-002",
+				},
+				Principal:  "User:u-222bbb",
+				RoleName:   "EnvironmentAdmin",
+				CrnPattern: "crn://confluent.cloud/organization=1111aaaa-11aa-11aa-11aa-111111aaaaaa/environment=env-aaa1111",
+			},
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/iam/v2/role-bindings", r.URL.Path)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(expectedList)
+	}))
+	defer ts.Close()
+
+	client := ccloud.NewClient().WithAuth(noopAuth{}).WithBaseUrl(ts.URL)
+
+	query := &ccloud.ListRoleBindingsQuery{
+		Principal:  "User:u-111aaa",
+		RoleName:   "CloudClusterAdmin",
+		CrnPattern: "crn://confluent.cloud",
+	}
+
+	result, err := client.ListRoleBindings(query)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	assert.Len(t, result.Data, 2)
+	assert.Equal(t, "rb-001", result.Data[0].Id)
+	assert.Equal(t, "User:u-111aaa", result.Data[0].Principal)
+}
+
+func TestListRoleBindingsError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "server error"}`))
+	}))
+	defer ts.Close()
+
+	client := ccloud.NewClient().WithAuth(noopAuth{}).WithBaseUrl(ts.URL)
+
+	result, err := client.ListRoleBindings(&ccloud.ListRoleBindingsQuery{})
+
+	assert.Error(t, err)
+	assert.Nil(t, result)
+	assert.Contains(t, err.Error(), "failed to list role bindings")
+}
+
+func TestGetRoleBinding(t *testing.T) {
+	expectedBinding := &ccloud.RoleBinding{
+		BaseModel: common.BaseModel{
+			ApiVersion: "iam/v2",
+			Kind:       "RoleBinding",
+			Id:         "rb-12345",
+		},
+		Principal:  "User:u-111aaa",
+		RoleName:   "CloudClusterAdmin",
+		CrnPattern: "crn://confluent.cloud/organization=1111aaaa-11aa-11aa-11aa-111111aaaaaa/environment=env-aaa1111/cloud-cluster=lkc-1111aaa",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/iam/v2/role-bindings/rb-12345", r.URL.Path)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(expectedBinding)
+	}))
+	defer ts.Close()
+
+	client := ccloud.NewClient().WithAuth(noopAuth{}).WithBaseUrl(ts.URL)
+
+	result, err := client.GetRoleBinding("rb-12345")
+
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, expectedBinding.Id, result.Id)
+	assert.Equal(t, expectedBinding.Principal, result.Principal)
+	assert.Equal(t, expectedBinding.RoleName, result.RoleName)
+}
+
+func TestGetRoleBindingNotFound(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"error": "not found"}`))
+	}))
+	defer ts.Close()
+
+	client := ccloud.NewClient().WithAuth(noopAuth{}).WithBaseUrl(ts.URL)
+
+	result, err := client.GetRoleBinding("rb-nonexistent")
+
+	assert.Error(t, err)
+	assert.Nil(t, result)
+	assert.Contains(t, err.Error(), "failed to get role binding")
+}
